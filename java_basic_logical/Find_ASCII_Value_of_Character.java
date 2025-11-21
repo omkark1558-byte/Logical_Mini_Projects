bcaf93dc1bfd58ad37931158a61c309dbbcf0610
omkark1558-byte <omkark1558@gmail.com>
2025-11-21 21:50:16 +0530

    Concept Basic :

---
+++ java_basic_logical/Find_ASCII_Value_of_Character.java
@@ -0,0 +1,13 @@
+package java_basic_logical;
+
+public class Find_ASCII_Value_of_Character {
+
+	public static void main(String[] args) {
+		 char ch = 'A';
+		 int ascii = ch;
+		 
+		 System.out.println(ascii);
+
+	}
+
+}
