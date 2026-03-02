3961923df18e5a7104c5bd17fddbf193342909d4
omkark1558-byte <omkark1558@gmail.com>
2026-03-02 23:46:00 +0530

    Logical Reverse Anagram

---
+++ anogram_logical_1/Revers_Array.java
@@ -0,0 +1,31 @@
+package anogram_logical_1;
+ 
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Revers_Array {
+
+	public static void main(String[] args) {
+		 
+		
+//		int[] arr = {14,53,776,89,07,894};
+//		
+//		for( int i= arr.length -1; i>=0; i--)
+//		{
+//			System.out.println(arr[i]);
+//		}
+		
+		List<Integer> list = Arrays.asList(10,63,54,673);
+		
+	
+		
+		Collections.reverse(list);
+		System.out.println(list);
+		
+		
+
+	}
+
+}
